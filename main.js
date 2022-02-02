73fcb7be1b55acb8b35f30251413cbf6e640fc03
AravindhKKS <kadaikuttysingam109@gmail.com>
2022-02-02 13:47:55 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// const form = document.querySelector('#form')
+// const name = document.querySelector('#un');
+// const mail = document.querySelector('#pw');
+// const msg = document.querySelector('#msg')
+
+// form.addEventListener('submit', onSubmit);
+
+// function onSubmit(e){
+   
+//     preventDefault();
+
+//     if(name.value ===''|| mail.value === '' ){
+//       msg.classList.add('error');
+//       msg.innerHTML = 'Please Enter All Fields';
+
+//       msg.style.color = "yellow"
+
+//       setTimeout(()=> msg.remove(), 2000);
+      
+//     } else{
+//         console.log(name.value);
+//         console.log(mail.value);
+//         console.log('Submit is Successfully');
+//     }
+  
+//   }
+   
+
+
